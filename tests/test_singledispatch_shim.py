b8399712ee56842bcc7e83e1be3ebaed6b885d5e
Eckhart Arnold <eckhart.arnold@posteo.de>
2022-04-24 21:42:24 +0200

    work in progress

---
+++ tests/test_singledispatch_shim.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""test_singledispatch_shim.py -- test code for ts2python's singledispatch"""
+
+
+from ts2python.singledispatch_shim import singledispatch, singledispatchmethod
+
+
+@singledispatch
+def func(arg):
+    raise TypeError(f"Possible types for arg are int or str, not {type(arg)}")
+
+
+@func.register
+def _(intarg: int):
+    assert isinstance(intarg, int)
+    return int
+
+
+@func.register
+def _(strarg: str):
+    assert isinstance(strarg, str)
+    return str
+
+
+class TestSingleDispatchShim:
+    def test_singledispatch(self):
+        assert func(1) is int
+        assert func("1") is str
+        try:
+            func([1, 2, 3])
+            assert False, "TypeError expected"
+        except TypeError:
+            pass
+
+
+if __name__ == "__main__":
+    from runner import runner
+    runner("", globals())

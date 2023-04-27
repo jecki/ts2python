41e751b539c6910b2ff2984549a09d4b26c1fc81
eckhart <eckhart.arnold@posteo.de>
2023-04-27 22:06:07 +0200

    #7 extends type examples added

--- ts2pythonParser.py
+++ ts2pythonParser.py
@@ -118,7 +118,7 @@ class ts2pythonGrammar(Grammar):
     literal = Forward()
     type = Forward()
     types = Forward()
-    source_hash__ = "e1d0a2a762dd4f1cff1562758277347b"
+    source_hash__ = "16c17eaa3770bfeab1670411e5e4ad53"
     disposable__ = re.compile('(?:INT$|NEG$|FRAC$|DOT$|EXP$|EOF$|_array_ellipsis$|_top_level_assignment$|_top_level_literal$|_quoted_identifier$|_root$|_namespace$|_part$)|(?:INT$|NEG$|FRAC$|DOT$|EXP$|EOF$|_array_ellipsis$|_top_level_assignment$|_top_level_literal$|_quoted_identifier$|_root$|_namespace$|_part$)')
     static_analysis_pending__ = []  # type: List[bool]
     parser_initialization__ = ["upon instantiation"]

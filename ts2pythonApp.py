f85b046ae5b874d4faa0bc18457d19d2de865fdf
eckhart <eckhart.arnold@posteo.de>
2025-06-27 14:54:17 +0200

    small changes to the app

--- ts2pythonApp.py
+++ ts2pythonApp.py
@@ -69,8 +69,8 @@ class ts2pythonApp(tkinter.Tk):
         self.progressbar = ttk.Progressbar(self, orient="horizontal", variable=self.progress)
         self.cancel = ttk.Button(self, text="Cancel", command=self.on_cancel)
         self.cancel['state'] = tkinter.DISABLED
-        self.result_info = ttk.Label(self, text='Result:')
-        self.result = tkinter.Text(self, bg="white")
+        self.errors_info = ttk.Label(self, text='Errors:')
+        self.errors = tkinter.Text(self, bg="white")
         self.exit = ttk.Button(text="Quit", command=self.on_close)
 
     def connect_events(self):
@@ -85,20 +85,20 @@ class ts2pythonApp(tkinter.Tk):
         self.pick_source.grid(row=0, column=0, columnspan=2)
         self.progressbar.grid(row=1, column=0, **padWE)
         self.cancel.grid(row=1, column=1, **padE)
-        self.result_info.grid(row=2, column=0, **padW)
-        self.result.grid(row=3, column=0, columnspan=2, **padAll)
+        self.errors_info.grid(row=2, column=0, **padW)
+        self.errors.grid(row=3, column=0, columnspan=2, **padAll)
         self.exit.grid(row=4, column=1, **padE)
         self.rowconfigure(3, weight=1)
         self.columnconfigure(0, weight=9)
 
     def clear_result(self):
         with self.lock:
-            self.result.delete("1.0", tkinter.END)
+            self.errors.delete("1.0", tkinter.END)
             self.update()
 
     def log_callback(self, txt):
-        self.result.insert(tkinter.END, txt + '\n')
-        self.result.yview_moveto(1.0)
+        self.errors.insert(tkinter.END, txt + '\n')
+        self.errors.yview_moveto(1.0)
         self.num_compiled += 1
         self.progress.set(min(100, int(100 * self.num_compiled / self.num_sources)))
         self.update()
@@ -115,11 +115,11 @@ class ts2pythonApp(tkinter.Tk):
         else:
             self.cancel['stat'] = tkinter.DISABLED
             if self.cancel_flag:
-                self.result.yview_moveto(1.0)
+                self.errors.yview_moveto(1.0)
                 with self.lock:  self.cancel_flag = False
             else:
-                self.result.insert(tkinter.END, "Compilation finished.\n")
-                self.result.insert(tkinter.END, f"Results written to {self.outdir}.\n")
+                self.errors.insert(tkinter.END, "Compilation finished.\n")
+                self.errors.insert(tkinter.END, f"Results written to {self.outdir}.\n")
                 if len(self.names) == 1:
                     basename = os.path.splitext(os.path.basename(self.names[0]))[0]
                     for msgtype in ('_ERRORS.txt', '_WARNINGS.txt'):
@@ -129,12 +129,15 @@ class ts2pythonApp(tkinter.Tk):
                             lines = msg.split('\n')
                             leadout = '...\n' if len(lines) > 20 else '\n'
                             msg = '\n'.join([msgtype[1:-4] ] + lines[:20] + [leadout])
-                            self.result.insert(tkinter.END, msg)
+                            self.errors.insert(tkinter.END, msg)
                             break
+                else:
+                    self.errors.insert(tkinter.END,
+                        f"Errors (if any) written to {self.outdir}.\n")
                 if self.target == 'html':
                     html_name = os.path.splitext(os.path.basename(self.names[0]))[0] + '.html'
                     html_name = os.path.join(self.outdir, html_name)
-                    self.result.insert(tkinter.END, html_name + "\n")
+                    self.errors.insert(tkinter.END, html_name + "\n")
                     webbrowser.open('file://' + os.path.abspath(html_name)
                                     if sys.platform == "darwin" else html_name)
                 else:
@@ -172,14 +175,14 @@ class ts2pythonApp(tkinter.Tk):
                         "Cancel running process?"):
                 if self.worker:
                     with self.lock:  self.cancel_flag = True
-                    self.result.insert(tkinter.END, "Canceling reaming tasks...\n")
+                    self.errors.insert(tkinter.END, "Canceling reaming tasks...\n")
                     self.update()
                     self.update_idletasks()
                     self.worker.join(5.0)
                     if not self.worker.is_alive():
-                        self.result.insert(tkinter.END, "Stopped.\n")
+                        self.errors.insert(tkinter.END, "Stopped.\n")
                         self.cancel_flag = False
-                    self.result.yview_moveto(1.0)
+                    self.errors.yview_moveto(1.0)
                     return True
                 else:
                     with self.lock:  self.cancel_flag = False
@@ -189,8 +192,8 @@ class ts2pythonApp(tkinter.Tk):
     def on_close(self):
         if self.on_cancel():
             if self.worker and self.worker.is_alive():
-                self.result.insert(tkinter.END, "Killing still running processes!\n")
-                self.result.yview_moveto(1.0)
+                self.errors.insert(tkinter.END, "Killing still running processes!\n")
+                self.errors.yview_moveto(1.0)
             self.destroy()
             self.quit()
 
